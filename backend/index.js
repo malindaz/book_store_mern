c796ab3cbc20b0e6800b3d057bc017fb4476a83b
Malinda Umesh <malindaumesh093@gmail.com>
2024-01-31 22:05:54 +0530

    g

--- backend/index.js
+++ backend/index.js
@@ -40,7 +40,7 @@ app.post('/books', async (request, response) => {
         response.status(500).send({message: error.message});
     }
 });
-//dfd
+//dfdfffff
 app.get('/books', async (request, response) => {
     try {
         const books = await Book.find({});
