26c0778dabe541d84781f508360d3dcb5c3cad09
Malinda Umesh <malindaumesh093@gmail.com>
2024-02-01 11:20:15 +0530

    1

--- backend/index.js
+++ backend/index.js
@@ -40,18 +40,56 @@ app.post('/books', async (request, response) => {
         response.status(500).send({message: error.message});
     }
 });
-//dfdfffff
+
 app.get('/books', async (request, response) => {
     try {
         const books = await Book.find({});
 
-        return response.status(200).json(books);
+        return response.status(200).json({
+            count: books.length,
+            data: books
+        });
     }catch (error){
         console.log(error.message);
         response.status(500).send({ message: error.message});
 
     }
 });
+
+app.get('/books/:id', async (request, response) => {
+    try {
+
+        const { id } = request.params;
+
+        const book = await Book.findById(id);
+
+        return response.status(200).json(book);
+    }catch (error){
+        console.log(error.message);
+        response.status(500).send({ message: error.message});
+
+    }
+});
+
+app.put('/book/:id', async (request, response) => {
+    try {
+        if(
+        !request.body.title ||
+        !request.body.author ||
+        !request.body.publishYear
+        ){
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishYear',
+            
+            });
+        } 
+        const { id } = request.params;
+
+    } catch (error) {
+        con
+    }
+})
+
 
 
 mongoose
